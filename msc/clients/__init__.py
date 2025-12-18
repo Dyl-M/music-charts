0d7fac5399914771a7699cdf1650dfd4c4ec4fc1
dyl_m <dyl_m.dev@proton.me>
2025-12-19 00:23:16 +0100

    feat(clients): export MusicBeeClient in package __init__
    * Add MusicBeeClient to clients package exports
    * Update __all__ list for public API

--- msc/clients/__init__.py
+++ msc/clients/__init__.py
@@ -2,7 +2,8 @@
 
 # Local
 from msc.clients.base import BaseClient
+from msc.clients.musicbee import MusicBeeClient
 from msc.clients.songstats import SongstatsClient
 from msc.clients.youtube import YouTubeClient
 
-__all__ = ["BaseClient", "SongstatsClient", "YouTubeClient"]
+__all__ = ["BaseClient", "MusicBeeClient", "SongstatsClient", "YouTubeClient"]
